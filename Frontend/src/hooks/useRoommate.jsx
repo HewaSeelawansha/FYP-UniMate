d70fc7c632df1a32435483c24ffe4ba1c24ddcb2
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-02-23 00:12:46 +0530

    Hook for students who looking roommate

--- Frontend/src/hooks/useRoommate.jsx
+++ Frontend/src/hooks/useRoommate.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import useAxiosPublic from './useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
+import useAuth from './useAuth';
 
 const useRoommateUsers = () => {
+    const {user} =  useAuth();
     const axiosPublic = useAxiosPublic();
 
     const { data: users = [], isPending: loading, refetch } = useQuery({
-        queryKey: ['roommateUsers'],
+        queryKey: [user?.email, 'roommateUsers'],
         queryFn: async () => {
-            const res = await axiosPublic.get('users/roommates');
+            if (!user) return [];
+            try {
+            const res = await axiosPublic.get(`users/roommates/${user.email}`);
             return res.data; 
+            } catch (error) {
+                console.error(error);
+                return [];
+            }
         },
     });
 
