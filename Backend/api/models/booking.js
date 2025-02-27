3678e4b21bbee36fc2f48d9b8f038a542ebbf8b3
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-02-27 15:47:06 +0530

    Booking model

---
+++ Backend/api/models/booking.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookingSchema = new Schema({
+    listing: {
+        type: Schema.Types.ObjectId,
+        ref: 'Listing',
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true
+    },
+    movein: {
+        type: String, 
+        required: true
+    },
+    gender: {
+        type: String, 
+        required: true,
+        enum:['Male','Female']
+    },
+    payvia: {
+        type: String,
+        required: true
+    },
+    needs: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: 'Pending'
+    },
+    paystatus: {
+        type: String,
+        enum: ['Done', 'Not Yet'],
+        default: 'Not Yet'
+    },
+    payment: {
+        type: Number,
+        default: 0
+    },
+},  {
+    timestamps: true,
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
