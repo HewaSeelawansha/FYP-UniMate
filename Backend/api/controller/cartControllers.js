55ca1f64bd9f03772093592ca360bcd7491d7f59
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-03-03 22:49:34 +0530

    cart controller

--- Backend/api/controller/cartControllers.js
+++ Backend/api/controller/cartControllers.js
@@ -67,15 +67,15 @@ const updateCart = async (req, res) => {
     }
 }
 // get single recipe
-const getSingleCart = async (req, res) => {
-    const cartId = req.params.id;
-    try {
-        const cartItem = await Carts.findById(cartId)
-        res.status(200).json(cartItem)
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-};
+// const getSingleCart = async (req, res) => {
+//     const cartId = req.params.id;
+//     try {
+//         const cartItem = await Carts.findById(cartId)
+//         res.status(200).json(cartItem)
+//     } catch (error) {
+//         res.status(500).json({message: error.message});
+//     }
+// };
 
 
 module.exports = {
