a774b963d05bddd5184a6079a1af2a7ca29f20ca
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-03-26 02:01:03 +0530

    use bookinh hook

---
+++ Frontend/src/hooks/useBooking.jsx
@@ -0,0 +1,25 @@
+// import React, { useContext } from 'react'
+// import { AuthContext } from '../contexts/AuthProvider'
+// import { useQuery } from '@tanstack/react-query';
+
+//   const useBooking = () => {
+//     const {user} = useContext(AuthContext);
+//     const token = localStorage.getItem('access-token');
+
+//     const { refetch, data:cart = []} = useQuery({
+//         queryKey: ['booking', user?.email],
+//         queryFn: async () => {
+//             const res = await fetch(`http://localhost:3000/carts?email=${user?.email}`,{
+//               headers: {
+//                 authorization : `Bearer ${token}`,
+//               },
+//               method: 'GET',
+//             })
+//             return res.json();
+//         },
+//     })
+  
+//   return [cart, refetch]
+// }
+
+// export default useCart
