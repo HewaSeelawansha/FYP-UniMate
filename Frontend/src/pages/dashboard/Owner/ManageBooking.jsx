a1591ef3d1e6c2f2b1d0f2feb0d709b347c5203e
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-03-29 01:00:00 +0530

    manage booking

--- Frontend/src/pages/dashboard/Owner/ManageBooking.jsx
+++ Frontend/src/pages/dashboard/Owner/ManageBooking.jsx
@@ -4,8 +4,10 @@ import { FcViewDetails } from "react-icons/fc";
 import { Link } from 'react-router-dom'
 import Swal from 'sweetalert2'
 import useAxiosSecure from '../../../hooks/useAxiosSecure'
+import useAuth from '../../../hooks/useAuth';
 
 const ManageBooking = () => {
+  const {user} = useAuth();
   const [bookings, setBookings] = useState([]);
   const axiosSecure = useAxiosSecure();
   const [nstatus, setNstatus] = useState('Pending');
@@ -13,7 +15,7 @@ const ManageBooking = () => {
 
   const fetchBooking = async () => {
     try {
-    const response = await fetch(`http://localhost:3000/booking/67be0c2ba758da62d427ab6d`);
+    const response = await fetch(`http://localhost:3000/booking/owner/${user.email}`);
     if (!response.ok) {
         throw new Error(`Failed to fetch bookings`);
     }
@@ -24,23 +26,23 @@ const ManageBooking = () => {
     } finally {
     setLoading(false);
     }
-};
+  };
 
-// useEffect(() => {
-//     fetchBooking();
-// }, []);
+  useEffect(() => {
+      fetchBooking();
+  }, []);
 
-const refetchReview = () => {
-    setLoading(true);
-    fetchBooking();
-}
+  const refetchReview = () => {
+      setLoading(true);
+      fetchBooking();
+  }
 
   const handleStatus = async (item) => {
     const data = {
       status: nstatus || item.status,
     };
     try {
-      const response = await axiosSecure.patch(`/boarding/status/${item._id}`, data);
+      const response = await axiosSecure.patch(`/booking/status/${item._id}`, data);
       if (response.data) {
         Swal.fire({
           position: 'center',
@@ -49,7 +51,7 @@ const refetchReview = () => {
           showConfirmButton: false,
           timer: 1500,
         });
-        refetch();
+        refetchReview();
       }
     } catch (error) {
       Swal.fire({
@@ -75,7 +77,14 @@ const refetchReview = () => {
             <thead className='bg-green text-white'>
             <tr>
                 <th>#</th>
-                <th>Email</th>
+                <th>Date</th>
+                <th>Sender</th>
+                <th>Move In</th>
+                <th>Needs</th>
+                <th>Pay Via</th>
+                <th>Pay Status</th>
+                <th>Paid</th>
+                <th>Amount</th>
                 <th>View</th>
                 <th>Status</th>
             </tr>
@@ -86,7 +95,21 @@ const refetchReview = () => {
                     <tr key={index}>
                         <th>{index + 1}</th>
 
+                        <td>{new Date(item.createdAt).toLocaleDateString()}</td>
+
                         <td>{item.email}</td>
+
+                        <td>{item.movein}</td>
+
+                        <td>{item.needs}</td>
+
+                        <td>{item.payvia}</td>
+
+                        <td>{item.paystatus}</td>
+
+                        <td>{item.paid}</td>
+
+                        <td>{item.payment}</td>
                         
                         <td>
                           <Link to={`/dashboard/view-boarding/${item.owner}`}><button className='btn btn-sm btn-circle text-green bg-black'><FcViewDetails/></button></Link>
