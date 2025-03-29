c6803ba65c35e21911ba054e27d91918a965cf9f
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-03-30 02:04:52 +0530

    booking routes

--- Backend/api/routes/bookingRoutes.js
+++ Backend/api/routes/bookingRoutes.js
@@ -5,9 +5,9 @@ const bookingController = require('../controller/bookingController');
 const verifyToken = require('../middleware/verifyToken');
 
 router.post('/', bookingController.postBooking)
-router.get('/:email/:id', bookingController.getBookigsByUserListing)
-router.get('/:id', bookingController.getBookigsByListing)
+router.get('/owner/:email', bookingController.getBookingsByOwner)
 router.get('/user/:email', bookingController.getBookigsByEmail)
+router.get('/:email/:id', bookingController.getBookigsByUserListing)
 router.patch('/:id', bookingController.updateBooking)
 router.patch('/status/:id', bookingController.updateStatus)
 router.delete('/:id', bookingController.deleteBooking)
