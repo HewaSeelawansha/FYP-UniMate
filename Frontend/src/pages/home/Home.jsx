06510596b4761918560aa45484d862c6ce864517
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-04-20 02:19:20 +0530

    homepage

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -6,17 +6,20 @@ import Testimonials from './Testimonials'
 import AmenitiesSpotlight from './AmenitiesSpotlight'
 import FeaturesGrid from './FeaturesGrid'
 import CallToAction from './CallToAction'
+import ActionBanner from './ActionBanner'
+import Services from './Services'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <ActionBanner/>
       <UniversityHubLocator/>
       <AmenitiesSpotlight/>
       <FeaturesGrid/>
       <PropertyShowcase/>
       <Testimonials/>
-      <CallToAction/>
+      <Services/>
     </div>
   )
 }
