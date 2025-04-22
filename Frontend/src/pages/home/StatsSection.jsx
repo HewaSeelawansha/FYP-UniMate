2ccd528f8dd7092e2f576c6ac93a09d7dde10d8f
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-04-22 23:48:57 +0530

    Improvements in homepage

---
+++ Frontend/src/pages/home/StatsSection.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+const StatsSection = () => {
+  return (
+    <section className="container shadow-md hover:shadow-2xl border border-green-500 rounded-lg mx-auto py-4">
+      <div className=" px-4">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
+          <div className="p-6">
+            <CountUp end={1000} duration={10} className="text-4xl font-bold text-green-500" />
+            <span className="text-4xl font-bold text-green-500">+</span>
+            <p className="mt-2 text-gray-900">Happy Students</p>
+          </div>
+          <div className="p-6">
+            <CountUp end={250} duration={10} className="text-4xl font-bold text-green-500" />
+            <span className="text-4xl font-bold text-green-500">+</span>
+            <p className="mt-2 text-gray-900">Verified Properties</p>
+          </div>
+          <div className="p-6">
+            <CountUp end={15} duration={10} className="text-4xl font-bold text-green-500" />
+            <span className="text-4xl font-bold text-green-500">+</span>
+            <p className="mt-2 text-gray-900">Property Owners</p>
+          </div>
+          <div className="p-6">
+            <CountUp end={24} duration={10} suffix="hr" className="text-4xl font-bold text-green-500" />
+            <span className="text-4xl font-bold text-green-500">+</span>
+            <p className="mt-2 text-gray-900">Average Move-in Time</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default StatsSection;
