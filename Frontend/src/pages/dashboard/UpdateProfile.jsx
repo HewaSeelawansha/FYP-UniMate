f7113b3761ad967751d60ca2441d99cd77a162af
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-04-24 01:45:09 +0530

    styling improvements

--- Frontend/src/pages/dashboard/UpdateProfile.jsx
+++ Frontend/src/pages/dashboard/UpdateProfile.jsx
@@ -36,12 +36,8 @@ const UpdateProfile = () => {
 
   const fetchPerson = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/users/${user.email}`);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch user: ${response.statusText}`);
-      }
-      const data = await response.json();
-      setPerson(data);
+      const response = await axiosSecure.get(`/users/${user.email}`);
+      setPerson(response.data);
     } catch (error) {
       console.error('Error fetching user:', error);
     } finally {
