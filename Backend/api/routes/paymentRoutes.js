9b3f57fd8ffd42ce68087688255a66c22efc53d6
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-04-26 13:55:33 +0530

    payment routes

--- Backend/api/routes/paymentRoutes.js
+++ Backend/api/routes/paymentRoutes.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controller/paymentControllers');
 const verifyToken = require('../middleware/verifyToken');
+const verifyAdmin = require('../middleware/verifyAdmin')
 
-//post payments to the db
-router.post('/', verifyToken, paymentController.postPaymentItem)
+//get all paymets
+router.get('/', verifyToken, verifyAdmin, paymentController.getAllPayments)
 
 // Get all payments for a specific student email
-router.get('/', verifyToken, paymentController.getPayements)
+router.get('/:email', verifyToken, paymentController.getPayementsStudent)
 
 // Gey payments associated to specific owner
 router.get('/owner/:email', paymentController.getPaymentsByListing);
 
+//post payments to the db
+router.post('/', verifyToken, paymentController.postPaymentItem)
 
 module.exports = router;
