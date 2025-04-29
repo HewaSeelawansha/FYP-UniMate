26b4c803bdd7aa27cebe0d5c8b80da116bdb191f
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-04-30 03:01:54 +0530

    Banner component homepage

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import bannerImg from '/BannerBGG.png';
+import bannerImg from '/HomeBG1.png';
 import { Link } from 'react-router-dom';
 
 const HeroBanner = () => {
