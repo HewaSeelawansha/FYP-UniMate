ae3550827690ba01997a4dfd15db4df2f9b63a31
HewaSeelawansha <10899426@students.plymouth.ac.uk>
2025-04-30 03:02:13 +0530

    Navbar component

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import logo from '/logounimate.png';
+import logo from '/logon.png';
 import { FaRegUser, FaSearch } from 'react-icons/fa';
 import { IoMdChatboxes } from "react-icons/io";
 import Modal from './Modal';
@@ -7,13 +7,35 @@ import Profile from './Profile';
 import { Link, useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import SearchModal from './SearchModal'; 
+import useAxiosSecure from '../hooks/useAxiosSecure';
 
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const [isScrolled, setIsScrolled] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false); 
   const navigate = useNavigate();
+  const axiosSecure = useAxiosSecure();
+  const [person, setPerson] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Only run if user exists and has an email
+    if (user?.email) {
+      const fetchPerson = async () => {
+        setIsLoading(true);
+        try {
+          const response = await axiosSecure.get(`/users/${user.email}`);
+          setPerson(response.data);
+        } catch (error) {
+          console.error('Error fetching user:', error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      fetchPerson();
+    }
+  }, [user?.email]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -50,7 +72,7 @@ const Navbar = () => {
           {/* Logo and Mobile Menu */}
           <div className="navbar-start">
             <Link to='/' className="flex items-center">
-              <img src={logo} alt="Logo" className="h-10 md:h-12 transition-all duration-300 hover:scale-110" />
+              <img src={logo} alt="Logo" className="h-10 md:h-12 transition-all duration-300 hover:scale-105" />
             </Link>
           </div>
 
@@ -75,9 +97,11 @@ const Navbar = () => {
             </Link>
 
             {/* User Button */}
-            {user ? (
-              <Profile user={user} />
-            ) : (
+            {user && person ? (
+              <Profile user={user} role={person.role} />
+            ) : isLoading? 
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2  border-green-500"></div>
+            : (
               <button 
                 onClick={() => document.getElementById('my_modal_5').showModal()} 
                 className="btn text-white hover:bg-emerald-600 bg-emerald-500 rounded-full px-6 text-white flex items-center gap-2 border-none shadow-md hover:shadow-lg transition-all duration-300"
